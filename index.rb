ddb24285afb767dd1aba8bebbffb29941b3406f4
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2023-05-25 20:43:30 -0700

    Complete required methods for both Node and LinkedList classes and their tests

--- index.rb
+++ index.rb
@@ -17,12 +17,22 @@ class Node
 end
 
 class LinkedList
-  def initialize(head = nil)
-    @head = head
+  def initialize(node_values = [])
+    @head = nil
     @tail = nil
     @size = 0
 
-    # TODO - loop to last node and point @tail to it
+    prev = nil
+    last_pos = node_values.size - 1
+
+    node_values.each_index do |i|
+      curr = Node.new(node_values[i])
+      @size += 1
+      @head = curr if i == 0
+      @tail = curr if i == last_pos
+      prev.next_node = curr if prev
+      prev = curr
+    end
   end
 
   attr_accessor(:head)
@@ -30,15 +40,37 @@ class LinkedList
   attr_accessor(:size)
 
   def append(value)
-    # TODO
+    node = Node.new(value)
+
+    if @size == 0
+      @head = node
+      @tail = node
+      @size += 1
+      return
+    end
+
+    @tail.next_node = node
+    @tail = node
+    @size += 1
   end
 
   def prepend(value)
-    # TODO
+    node = Node.new(value)
+
+    if @size == 0
+      @head = node
+      @tail = node
+      @size += 1
+      return
+    end
+
+    node.next_node = @head
+    @head = node
+    @size += 1
   end
 
   def size
-    # TODO
+    @size
   end
 
   def head
@@ -50,31 +82,92 @@ class LinkedList
   end
 
   def at(index)
-    # TODO
+    return nil if index < 0 or index >= @size or @size == 0
+
+    curr = @head
+    i = 0
+    while i < @size
+      return curr if i == index
+      curr = curr.next_node
+      i += 1
+    end
   end
 
   def pop
-    # TODO
+    return nil if @size == 0
+
+    if @size == 1
+      popped = @tail
+      @head = nil
+      @tail = nil
+      @size -= 1
+      return popped
+    end
+
+    old_tail = @tail
+    new_tail = nil
+    curr = @head
+    prev = nil
+
+    while curr != old_tail
+      new_tail = curr
+      curr = curr.next_node
+    end
+
+    new_tail.next_node = nil
+    @tail = new_tail
+    @size -= 1
+
+    old_tail
   end
 
   def contains?(value)
-    # TODO
+    return false if @size == 0
+
+    curr = @head
+    while curr
+      return true if curr.value == value
+      curr = curr.next_node
+    end
+
+    false
   end
 
   def find(value)
-    # TODO
+    return nil if @size == 0
+
+    index = 0
+    curr = @head
+    while curr
+      return index if curr.value == value
+      curr = curr.next_node
+      index += 1
+    end
+
+    nil
   end
 
   def to_s
-    # TODO
+    return "nil" if @size == 0
+
+    res = []
+    curr = @head
+    while curr
+      res.push("( #{curr.value.to_s} ) -> ")
+      curr = curr.next_node
+    end
+    res.push("nil")
+    res.join
   end
 
   # extra credit (optional)
 
+  # TODO - write tests for it
   def insert_at(value, index)
     # TODO
   end
 
+  # TODO - write tests for it
   def remove_at(index)
     # TODO
   end
--- index_spec.rb
+++ index_spec.rb
@@ -29,9 +29,158 @@ end
 
 RSpec.describe 'LinkedList class' do
   describe "initialize" do
-    it "TODO" do
-      # TODO
-      expect(true).to eq(true)
+    it "creates an empty list if called with no arguments" do
+      list = LinkedList.new
+      expect(list.head).to eq(nil)
+      expect(list.tail).to eq(nil)
+      expect(list.size).to eq(0)
+    end
+
+    it "creates a filled list if called with an array of values" do
+      list = LinkedList.new([1, 2, 3])
+      expect(list.head.value).to eq(1)
+      expect(list.tail.value).to eq(3)
+      expect(list.size).to eq(3)
+    end
+  end
+
+  describe "append" do
+    it "adds a node with value 1 to the list's end if called with 1 on an empty list" do
+      list = LinkedList.new
+      list.append(1)
+      expect(list.head.value).to eq(1)
+      expect(list.tail.value).to eq(1)
+      expect(list.size).to eq(1)
+    end
+
+    it "adds a node with value 3 to the list's end if called with 3 on a 2-sized list" do
+      list = LinkedList.new([1, 2])
+      list.append(3)
+      expect(list.head.value).to eq(1)
+      expect(list.tail.value).to eq(3)
+      expect(list.size).to eq(3)
+    end
+  end
+
+  describe "prepend" do
+    it "inserts a node with value 1 to the list's start if called with 1 on an empty list" do
+      list = LinkedList.new
+      list.prepend(1)
+      expect(list.head.value).to eq(1)
+      expect(list.tail.value).to eq(1)
+      expect(list.size).to eq(1)
+    end
+
+    it "inserts a node with value 0 to the list's start if called with 0 on a 2-sized list" do
+      list = LinkedList.new([1, 2])
+      list.prepend(0)
+      expect(list.head.value).to eq(0)
+      expect(list.tail.value).to eq(2)
+      expect(list.size).to eq(3)
+    end
+  end
+
+  describe "at" do
+    it "returns nil if called with a negative index" do
+      list = LinkedList.new
+      expect(list.at(-1)).to eq(nil)
+    end
+
+    it "returns nil if called with an out-of-bounds index" do
+      list = LinkedList.new
+      expect(list.at(1)).to eq(nil)
+    end
+
+    it "returns nil if called on an empty list" do
+      list = LinkedList.new
+      expect(list.at(0)).to eq(nil)
+    end
+
+    it "returns the middle node if called with 1 on a 3-sized list" do
+      list = LinkedList.new([1,2,3])
+      expect(list.at(1).value).to eq(2)
+    end
+  end
+
+  describe "pop" do
+    it "returns nil if called on an empty list" do
+      list = LinkedList.new
+      res = list.pop
+      expect(res).to eq(nil)
+      expect(list.head).to eq(nil)
+      expect(list.tail).to eq(nil)
+      expect(list.size).to eq(0)
+    end
+
+    it "removes and returns the last node if called on a 2-sized list" do
+      list = LinkedList.new([1, 2])
+      res = list.pop
+      expect(res.value).to eq(2)
+      expect(list.head.value).to eq(1)
+      expect(list.tail.value).to eq(1)
+      expect(list.size).to eq(1)
+    end
+
+    it "removes and returns the last node if called on a 1-sized list" do
+      list = LinkedList.new([1])
+      res = list.pop
+      expect(res.value).to eq(1)
+      expect(list.head).to eq(nil)
+      expect(list.tail).to eq(nil)
+      expect(list.size).to eq(0)
+    end
+  end
+
+  describe "contains?" do
+    it "returns false if called with any value on an empty list" do
+      list = LinkedList.new
+      expect(list.contains?(0)).to eq(false)
+    end
+
+    it "returns false if called with a value that is not in the non-empty list" do
+      list = LinkedList.new([1,2,3])
+      expect(list.contains?(0)).to eq(false)
+    end
+
+    it "returns true if called with a value that is in the non-empty list" do
+      list = LinkedList.new([1,2,3])
+      expect(list.contains?(2)).to eq(true)
+    end
+  end
+
+  describe "find" do
+    it "returns nil if called with any value on an empty list" do
+      list = LinkedList.new
+      expect(list.find(0)).to eq(nil)
+    end
+
+    it "returns nil if called with a value that is not in the non-empty list" do
+      list = LinkedList.new([1,2,3])
+      expect(list.find(0)).to eq(nil)
+    end
+
+    it "returns the correct index if called with a value that is in the non-empty list" do
+      list = LinkedList.new([1,2,3])
+      expect(list.find(2)).to eq(1)
+    end
+  end
+
+  describe "to_s" do
+    it "returns 'nil' if called on an empty list" do
+      list = LinkedList.new
+      expect(list.to_s).to eq("nil")
+    end
+
+    it "returns the correct string if called on a list of size 1" do
+      list = LinkedList.new([1])
+      res = "( 1 ) -> nil"
+      expect(list.to_s).to eq(res)
+    end
+
+    it "returns the correct string if called on a list of size 3" do
+      list = LinkedList.new([1,2,3])
+      res = "( 1 ) -> ( 2 ) -> ( 3 ) -> nil"
+      expect(list.to_s).to eq(res)
     end
   end
 end
